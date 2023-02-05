d77fa214a378a0474e6d5b1a34ec7ed96ca86e74
naveenvarma600 <naveenvarma600@gmail.com>
2023-02-05 11:22:00 +0530

    added parking lot lld

--- Parking Lot/CustomDataType/Person.java
+++
@@ -1,9 +0,0 @@
-package LLD.Concept_And_Coding.L6_Parking_Lot.CustomDataType;
-
-
-public class Person {
-    private String name;
-    private String address;
-    private String phone;
-    private String email;
-}
